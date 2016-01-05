05fe2328f50327971236017be7cce8d48ebe418c
lrobanthro <lrobinson@anthropologie.com>
2016-01-05 09:26:28 -0500

    initial dog

---
+++ swift_class.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Dog {
+    var weight: Int = 0
+    var height: Int = 0
+    let numberOfLegs = 4
+    
+    
+    
+}
+let testDog = Dog()
+print(testDog.numberOfLegs)
